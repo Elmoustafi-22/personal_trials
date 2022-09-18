7f0c03b2e877c2a7a60fcf922b111c24c9ed9b9b
mustopha <mustophaqadir@gmail.com>
2022-09-18 01:30:52 -0700

    square

---
+++ 7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - that draws a diagonal line
+ * @n: the number of times \ should be printed
+ * Return: success
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	int i, j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 1)
+				_putchar('\\');
+			else if (j < i)
+				_putchar(' ');
+		}
+	_putchar('\n');
+	}
+	}
+}
